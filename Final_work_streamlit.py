a1a9efa9a7986da6259b720b9d64e35725f7da57
NelliKS <95210661+NelliKS@users.noreply.github.com>
2024-10-31 15:35:55 +0200

    Update Final_work_streamlit.py

--- Final_work_streamlit.py
+++ Final_work_streamlit.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import streamlit as st
 
-url = 'https://raw.githubusercontent.com/NelliKS/Final_project/refs/heads/main/Electricity_20-09-2024.csv'
-url2 = 'https://raw.githubusercontent.com/NelliKS/Final_project/refs/heads/main/sahkon-hinta-010121-240924.csv'
+url = './Electricity_20-09-2024.csv'
+url2 = './sahkon-hinta-010121-240924.csv'
 df_electricity = pd.read_csv(url, delimiter=';', decimal = ',')
 
 df_price = pd.read_csv(url2)
